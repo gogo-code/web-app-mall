5417491c48fbd4a7ab07ea19a5d553b4fcaccc1c
gogo-code <3059349417@qq.com>
2020-08-17 18:49:47 +0800

    初始化

---
+++ src/views/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>asfd</h2>
+</template>
+
+<script>
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import { getHomeMultidata } from "network/home";
+export default {
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+
+    getHomeMultidata().then(res => {
+      console.log(res.data.banner.list)
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+import {Swiper,SwiperItem} from 'components/common/swiper'
+export default {
+  name:'',
+  props: {
+    banners: {
+      type:Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
