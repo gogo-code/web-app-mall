6d7ebe1b0ebc341a4535a2505c84bdf7ba8525cf
gogo-code <3059349417@qq.com>
2020-08-20 14:44:08 +0800

    商品请求更新

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="(item, index) in goods" :key="index" :goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    GoodsListItem,
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.goods {
+  display: flex;
+  /* 设置允许多行  主轴空白环绕*/
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 2px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,53 +1,112 @@
 <template>
   <div id="home">
-        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-        <home-swiper :banners="banners"></home-swiper>
-        <recommend-view :recommends="recommends"></recommend-view>
-        <feature-view></feature-view>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+    <good-list :goods="showGoods"></good-list>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import Scroll from "components/common/scroll/Scroll";
+import GoodList from "components/content/goods/GoodsList";
 
-import HomeSwiper from './childComps/HomeSwiper'
-import RecommendView from './childComps/RecommendView'
-import FeatureView from './childComps/FeatureView'
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
-import { getHomeMultidata,getHomeGoods} from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 export default {
   name: "Home",
-  components:{
+  components: {
     NavBar,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    FeatureView,
+    TabControl,
+    Scroll,
+    GoodList,
   },
   data() {
     return {
       banners: [],
       recommends: [],
       goods: {
-        'pop': {page:0,list:[]},
-        'new':{page:0,list:[]},
-        'sell':{page:0,list:[]},
-      }
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
     };
   },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
+  },
   created() {
-
-    getHomeMultidata().then(res => {
-      console.log(res.data.banner.list)
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+        case 2:
+          this.currentType = "sell";
+      }
+    },
+    /* 请求网络相关方法 */
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        console.log(this.goods);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        //this.$refs.scroll.finishPullUp()
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+  position: relative;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
 }
 </style>
